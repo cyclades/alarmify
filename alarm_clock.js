9eb388c168c2342497316aeb54b859299d002cb7
Alex Hu <alex@identified.com>
2012-04-14 22:34:51 -0700

    app will play song dropped in

--- alarm_clock.js
+++ alarm_clock.js
@@ -1,7 +1,33 @@
 //Time
 $(document).ready(function(){
+  //load the relevant libraries
+  
+  var sp          = getSpotifyApi(1);
+  var models      = sp.require("sp://import/scripts/api/models");
+  var views       = sp.require("sp://import/scripts/api/views");
+  var ui          = sp.require("sp://import/scripts/ui");
+  var player      = models.player;
+  var library     = models.library;
+  var application = models.application;
+  var playerImage = new views.Player();
+
+  var handleLinks = function() {
+    var links = models.application.links;
+    if(links.length) {
+      switch(links[0].split(":")[1]) {
+        default:
+          player.play(models.Track.fromURI(links[0]));
+          break;
+      };
+    };
+  };
+  application.observe(models.EVENT.LINKSCHANGED, handleLinks);
+
+  //update time object
   var selectedTime = $(".time").serializeArray();
   $(".time").change(function(){
     selectedTime = $(".time").serializeArray();
   });
+
+  handleLinks();
 });
